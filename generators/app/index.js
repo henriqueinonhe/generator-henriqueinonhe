718606148d143d270742efb746f84c08b27a3a62
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-06-19 13:46:49 -0300

    Test

--- generators/app/index.js
+++ generators/app/index.js
@@ -165,7 +165,7 @@ module.exports = class extends Generator {
   install() {
     const dependencies = ScaffoldingData(this.answers).dependencies;
     const devDependencies = ScaffoldingData(this.answers).devDependencies;
-    this.spawnCommandSync("npm", ["install", ...dependencies, "--legacy-peer-deps"]);
-    this.spawnCommandSync("npm", ["install", ...devDependencies, "--save-dev --legacy-peer-deps"]);
+    this.spawnCommandSync("npm", ["install", ...dependencies], "--legacy-peer-deps");
+    this.spawnCommandSync("npm", ["install", ...devDependencies], "--save-dev --legacy-peer-deps");
   }
 }
